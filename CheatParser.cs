4321f86c71e24ce34d146b9d8e042ea6927d7d34
CARVERD12 <36738000+CARVERD12@users.noreply.github.com>
2018-03-24 17:37:26 -0400

    Add files via upload

---
+++ CheatParser.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CheatParser : MonoBehaviour
+{
+    string cheatString;
+    string[] cheats = { "secret", "abcd", "space cheat" };   //Add your cheats here
+
+	void Update ()
+    {
+		if (Input.anyKeyDown)
+        {
+            cheatString = cheatString + Input.inputString.ToLower();
+            int possibleCheats = 0;
+            for (int i = 0; i < cheats.Length; i++)
+            {
+                if (cheats[i].StartsWith(cheatString)) possibleCheats += 1;
+                if (i == cheats.Length - 1) if (possibleCheats == 0) cheatString = null;    //Restart polling
+            }
+
+            switch(cheatString)
+            {
+                case "secret":
+                    Debug.Log("Secret Cheat");
+                    cheatString = null;
+                    break;
+                case "abcd":
+                    Debug.Log("Alphabet Cheat");
+                    cheatString = null;
+                    break;
+                case "space cheat":
+                    Debug.Log("Cheat With Space");
+                    cheatString = null;
+                    break;
+            }
+        }
+	}
+}
